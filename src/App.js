371da56d92f0c9dba674baf6a05543ea2aaa67b2
Junya Qiao <junyaqiao@gmail.com>
2022-01-13 19:13:42 -0500

    nav bar function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 // /import logo from './logo.svg';
 import './App.css';
-import Nav from './component/Nav';
-import Main from './component/Main';
-//import About from './component/About';
 
-function App() {
+//import About from './component/About';
+import Navuse from './utils/Navuse'
+
+const App=()=><Navuse />
+  /*
   return (
     <div>
        <Nav></Nav>
       <main>
-      {/*<About></About>*/}
+      {/*<About></About>}
        <Main></Main>
       </main>
     </div>
-  );
-}
+  );*/
+
 
 export default App;
